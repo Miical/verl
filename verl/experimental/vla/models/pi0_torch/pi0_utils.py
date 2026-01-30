d8a2fc8eff9e338af2e0cf102205a2b5e5d39062
Miical <879754743@qq.com>
2026-01-30 05:55:59 +0000

    renew copyright

--- verl/experimental/vla/models/pi0_torch/pi0_utils.py
+++ verl/experimental/vla/models/pi0_torch/pi0_utils.py
@@ -1,4 +1,5 @@
 # Copyright 2025 Bytedance Ltd. and/or its affiliates
+# Copyright 2025 Giga Team. and/or its affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# from https://github.com/open-gigaai/giga-models
+
 
 from typing import Any
 
